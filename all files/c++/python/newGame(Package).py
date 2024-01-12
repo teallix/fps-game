4fe9075413b8564c2a740c9ff2e8c9d0fe5b071a
teallix <119893045+teallix@users.noreply.github.com>
2024-01-12 16:13:00 +0000

    adding game(new)

---
+++ all files/c++/python/newGame(Package).py
@@ -0,0 +1,14 @@
+import time
+import random
+
+def gameStart():
+    enter = input("Enter your name to play game\n")
+    time.sleep(0.75)
+    print("Hello, " +enter+ ", this is a game designed to make you cry of confusion")
+    time.sleep(2.5)
+    print("Sunday, February 11th, 1996, 0400")
+    time.sleep(0.99)
+    print("17°51'50'N 77°17'53'W', off the coast of Jamaica") # reminder to change the '' to "" before realease
+    
+
+gameStart()
